2e360265451eaa1700837a8482b43faedde047bf
chayatep <chayatep01@hotmail.com>
2018-11-29 01:52:50 +0700

    My Expense style

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -3,8 +3,15 @@ import { connect } from 'react-redux';
 import { startLogin } from '../actions/auth';
 
 export const LoginPage = ({ startLogin }) => (
-  <div>
-    <button onClick={startLogin}>Login</button>
+
+  <div className="box-layout">
+
+      <div className="box-layout__box">
+        <h1 className=".box-layout__title"> My Expenses</h1>
+        <p> Get all expenses under control</p>
+        <button className="button" onClick={startLogin}>Login with Gmail</button>
+      </div>
+   
   </div>
 );
 
